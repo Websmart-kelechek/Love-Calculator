0b85e493e752d740eaee95c1f32ecb34985f5822
Daniyar Kerimkulov <daniyar.kerimkulov.kk@gmail.com>
2021-10-12 01:06:31 +0600

    change script

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let loveScore = Math.random() * 100;
 loveScore = Math.floor(loveScore) + 1;
 
 if (loveScore > 70) {
-  document.write("<h1>Your love score is " + loveScore + "%<br>" + " Your love each other like Raimaly loves Begimay!</h1>"); 
+  document.write("<h1>Your love score is " + loveScore + "%<br>" + " Your love each other like<br> Raimaly loves Begimay!</h1>"); 
 } else if (loveScore > 30) {
   document.write("<h1>Your love score is " + loveScore + "%<br>" + " Keep going!</h1>");
 } else {
